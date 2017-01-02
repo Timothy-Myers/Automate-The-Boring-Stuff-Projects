e8f6a3a5be188dd69e2090c9424b142a5e5104e1
Timothy-Myers <timothy.myers@maine.edu>
2017-01-02 15:03:28 -0500

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,13 @@
+print('How many cats do you have?')
+numCats = input() #gets the number of cats a user has
+try:
+    if int(numCats) >= 4:
+        print('You have a lot of cats..')
+    elif int(numCats) > 0 and int(numCats) < 4:
+        print('You have a regular amount of cats')
+    elif int(numCats) == 0:
+        print('You should get a cat')
+    elif int(numCats) < 0:
+        print('How does one have negative cats?')
+except ValueError:
+    print('Please enter a number.')
