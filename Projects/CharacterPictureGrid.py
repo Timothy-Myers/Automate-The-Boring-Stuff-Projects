ff70cf932a5165280c2d01e2b4a03320d4064a50
Timothy-Myers <timothy.myers@maine.edu>
2017-01-08 21:36:41 -0500

    Add files via upload

---
+++ Projects/CharacterPictureGrid.py
@@ -0,0 +1,12 @@
+#program that reads a specific grid and effectively rotates it
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', '0', '0', '.', '.', '.'],
+        ['0', '0', '0', '0', '.', '.'],
+        ['0', '0', '0', '0', '0', '.'],
+        ['.', '0', '0', '0', '0', '0'],
+        ['0', '0', '0', '0', '0', '.'],
+        ['0', '0', '0', '0', '.', '.'],
+        ['.', '0', '0', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+print('\n'.join(map(''.join, zip(*grid)))) #prints the rotated grid by utilizing the join and zip functions
